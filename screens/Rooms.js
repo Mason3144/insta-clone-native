e8cf3edf635f5d50e78a16ba5d6fc1183d55efb5
Mason3144 <lts890303@gmail.com>
2022-10-12 07:12:22 +0900

    Rooms UI

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -4,10 +4,43 @@ import { FlatList, Text, View } from "react-native";
 import { ROOM_FRAGMENT } from "../fragments";
 import styled from "styled-components";
 import ScreenLayout from "../components/ScreenLayout";
+import useMe from "../hooks/useMe";
+import { colors } from "../colors";
 
-const RoomContainer = styled.View``;
-const RoomText = styled.Text`
+const RoomContainer = styled.TouchableOpacity`
+  width: 100%;
+  padding: 15px 10px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Column = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+const Avatar = styled.Image`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  margin-right: 20px;
+`;
+const Data = styled.View``;
+const UnreadDot = styled.View`
+  width: 10px;
+  height: 10px;
+  border-radius: 50%;
+  background-color: ${colors.blue};
+`;
+const Username = styled.Text`
   color: white;
+  font-weight: 600;
+  font-size: 16px;
+`;
+const UnreadText = styled.Text`
+  color: white;
+  margin-top: 2px;
+  font-weight: 500;
 `;
 
 const SEE_ROOMS_QUERY = gql`
@@ -20,18 +53,41 @@ const SEE_ROOMS_QUERY = gql`
 `;
 export default function Rooms() {
   const { data, loading } = useQuery(SEE_ROOMS_QUERY);
-  console.log(data);
-  const renderItem = ({ item: room }) => (
-    <RoomContainer>
-      <RoomText>
-        {room.unreadTotal === "0" ? "Name" : `${room.unreadTotal} messages`}
-      </RoomText>
-    </RoomContainer>
-  );
+  const { data: meData } = useMe();
+  const renderItem = ({ item: room }) => {
+    const notMe = room.users.find(
+      (user) => user.username !== meData?.me?.username
+    );
+    return (
+      <RoomContainer>
+        <Column>
+          <Avatar source={{ uri: notMe.avatar }} />
+          <Data>
+            <Username>{notMe.username}</Username>
+            <UnreadText>
+              {room.unreadTotal} unread{" "}
+              {room.unreadTotal === 1 ? "message" : "messages"}
+            </UnreadText>
+          </Data>
+        </Column>
+        <Column>{room.unreadTotal !== 0 ? <UnreadDot /> : null}</Column>
+      </RoomContainer>
+    );
+  };
 
   return (
     <ScreenLayout loading={loading}>
       <FlatList
+        ItemSeparatorComponent={
+          <View
+            style={{
+              width: "100%",
+              height: 1,
+              backgroundColor: "rgba(255,255,255,0.2)",
+            }}
+          ></View>
+        }
+        style={{ width: "100%" }}
         data={data?.seeRooms}
         keyExtractor={(room) => "" + room.id}
         renderItem={renderItem}
