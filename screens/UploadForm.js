55b68ab3547f8f12e9c588b46b6eb4032da7bd2d
Mason3144 <lts890303@gmail.com>
2022-10-11 09:13:38 +0900

    UploadForm UI

--- screens/UploadForm.js
+++ screens/UploadForm.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { Text, View } from "react-native";
 import styled from "styled-components";
+import DismissKeyboard from "../components/DismissKeyboard";
+
+const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  padding: 0px 50px;
+`;
+const Photo = styled.Image`
+  height: 350px;
+`;
+const CaptionContainer = styled.View`
+  margin-top: 30px;
+`;
+const Caption = styled.TextInput`
+  background-color: white;
+  color: black;
+  padding: 10px 20px;
+  border-radius: 100px;
+`;
 
 export default function UploadForm({ route }) {
-  console.log(route);
   return (
-    <View>
-      <Text>asd</Text>
-    </View>
+    <DismissKeyboard>
+      <Container>
+        <Photo resizeMode="contain" source={{ uri: route?.params?.file }} />
+        <CaptionContainer>
+          <Caption
+            placeholder="Write a caption"
+            placeholderTextColor="rgba(0,0,0,0.3)"
+          />
+        </CaptionContainer>
+      </Container>
+    </DismissKeyboard>
   );
 }
