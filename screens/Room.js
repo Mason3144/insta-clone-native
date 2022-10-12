fdcfebe130d592aa0e23f607ac9772b2fbf0743c
Mason3144 <lts890303@gmail.com>
2022-10-12 09:46:35 +0900

    KeyboardAvoidingView for Direct message

--- screens/Room.js
+++ screens/Room.js
@@ -1,7 +1,26 @@
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
+import { FlatList, KeyboardAvoidingView } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { gql, useQuery } from "@apollo/client";
+import ScreenLayout from "../components/ScreenLayout";
+import styled from "styled-components";
+
+const MessageContainer = styled.View``;
+const Author = styled.View``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+const TextInput = styled.TextInput`
+  margin-bottom: 50px;
+  width: 95%;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 1000px;
+`;
 
 const SEE_ROOM_QUERY = gql`
   query seeRoom($id: Int!) {
@@ -26,7 +45,6 @@ export default function Room({ route, navigation }) {
       id: route?.params?.id,
     },
   });
-  console.log(data);
   useEffect(() => {
     navigation.setOptions({
       title: `${route?.params?.talkingTo?.username}`,
@@ -40,9 +58,35 @@ export default function Room({ route, navigation }) {
       ),
     });
   }, []);
+  const renderItem = ({ item: message }) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message.user.avatar }} />
+        <Username>{message.user.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
   return (
-    <View>
-      <Text>Room List</Text>
-    </View>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "black" }}
+      behavior="height"
+      keyboardVerticalOffset={70}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          inverted
+          style={{ width: "100%" }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => "" + message.id}
+          renderItem={renderItem}
+        />
+        <TextInput
+          placeholder="Write a message"
+          returnKeyLabel="Send Message"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 }
